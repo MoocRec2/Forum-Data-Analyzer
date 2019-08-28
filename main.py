86fc6645d18cb62e1caf7f67ca57d58c3aa6285b
Sameer Mohamed <smrbasil4@gmail.com>
2019-08-28 17:31:21 +0530

    Removed Unnecessary Code

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from forum_analyzer import analyze_course
 
-# Contains a MODERATE amount of data
-course_one = 'course-v1:Microsoft+DAT236x+1T2019a'  # 5 Threads
-
-# Contains a LARGE amount of data
-course_two = 'course-v1:UCSanDiegoX+DSE200x+1T2019a'  # 180 Threads
-
-course_four = 'course-v1:HarvardX+PH125.8x+2T2018'  # 99 Threads
-
 courses = [
+    'course-v1:HarvardX+PH125.8x+2T2018',  # 5 Threads
+    'course-v1:UCSanDiegoX+DSE200x+1T2019a',  # 180 Threads
+    'course-v1:Microsoft+DAT236x+1T2019a',  # 99 Threads
     'course-v1:CurtinX+IOT3x+2T2019',  # 15 threads
     'course-v1:MITx+6.002.3x+2T2019',  # 7 threads
     'course-v1:DelftXRWTHx+BioBased1x+2T2019'  # 38 threads
 ]
 
-# for course in courses:
-#     analyze_course(course)
+for course in courses:
+    analyze_course(course)
 
-analyze_course(course_two)
+# analyze_course(course_two)
